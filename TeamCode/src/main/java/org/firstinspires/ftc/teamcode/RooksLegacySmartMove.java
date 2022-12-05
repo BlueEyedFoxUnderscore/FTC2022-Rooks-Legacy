7ff03e53a2d510e1e0a2b6de0cdbc28cae5abdc6
Pagzma <pagzma09@gmail.com>
2022-12-04 17:50:08 -1000

    Added method to find hue ranges

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RooksLegacySmartMove.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RooksLegacySmartMove.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
 import android.graphics.Color;
-import android.graphics.ColorSpace;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
@@ -19,6 +18,7 @@ import com.sun.tools.javac.util.Pair;
 import org.firstinspires.ftc.robotcore.external.JavaUtil;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
+import kotlin.Triple;
 import ma.phoenix.ftc.realsensecamera.ConfigurableRealSenseCamera;
 import ma.phoenix.ftc.realsensecamera.FrameData;
 import ma.phoenix.ftc.realsensecamera.exceptions.CameraStartException;
@@ -31,6 +31,8 @@ import ma.phoenix.ftc.realsensecamera.exceptions.UnsupportedStreamTypeException;
 
 @TeleOp(name = "Rooks Legacy Smart Move")
 public class RooksLegacySmartMove extends LinearOpMode {
+
+  private ConfigurableRealSenseCamera camera = null;
 
   private DcMotor lift;
   private Servo claw;
@@ -51,8 +53,8 @@ public class RooksLegacySmartMove extends LinearOpMode {
   @Override
   public void runOpMode() {
 
-    try (ConfigurableRealSenseCamera camera = new ConfigurableRealSenseCamera(hardwareMap, () -> isStopRequested())) {
-
+    try {
+      camera = new ConfigurableRealSenseCamera(hardwareMap, () -> isStopRequested());
       ElapsedTime delay;
       int liftPosition;
       drive = new SampleMecanumDrive(hardwareMap);
@@ -134,7 +136,7 @@ public class RooksLegacySmartMove extends LinearOpMode {
           if (liftPosition == 3) moveLift(24.5);
           if (liftPosition == 4) moveLift(34.5);
 
-          if ((gamepad1.right_trigger > 0.5) || gamepad1.a) claw.setPosition(25.0 / 190); // In pressed pos
+          if ((gamepad1.right_trigger > 0.5) || gamepad1.a) claw.setPosition(30.0 / 190); // In pressed pos
           else claw.setPosition(12.0 / 190); // In release pos
 
           if (gamepad1.left_trigger > 0.5 || gamepad1.b) {
@@ -153,48 +155,53 @@ public class RooksLegacySmartMove extends LinearOpMode {
 
           //EXPERIMENTAL
           if (gamepad1.x) {
-            int i;
             if (!camera.updateFrameSet()) continue;
             FrameData data = camera.getImageFrame(StreamType.DEPTH);
-            int middleCone = -1;
-            float depth = 100000000;
-            int x = -1;
-            float[] hsv = new float[3];
             int scanlineY = (int) (data.getHeight() * 0.75);
-            for (i = (int) (data.getWidth() * 0.2); i < data.getWidth() * 0.8; i++) {
-              Color.colorToHSV(camera.getARGB(i, scanlineY), hsv);
-              double hue = hsv[0];
-              double sat = hsv[1];
-              double val = hsv[2];
-              if (camera.getDistance(i, scanlineY) != 0 && camera.getDistance(i, scanlineY) < depth) {
-                if ((hueRange(hue, 220, 260) || hueRange(hue, 320, 10)) && sat > 0.3) {
-                  x = i;
-                  depth = camera.getDistance(i, scanlineY);
-                }
-              }
-            }
+
+            int i;
+            int nearestPointGuess = -1;
 
 
+            float[] hsv = new float[3];
+
+            Pair<Float, Float>[] coneColourRanges = new Pair[]{
+                    new Pair<>(220f, 260f),
+                    new Pair<>(320f, 10f ) };
+
+            Triple<Integer, Integer, Float> positionAndHueRangeIndex = getNearestObjectWithHueRange(scanlineY, coneColourRanges);
+
+            int middleCone = -1;
             int leftSideCone = -1;
             int rightSideCone = -1;
             int notConeTolerance = 5;
             int notConeCountdown = notConeTolerance;
+            float depth;
 
-
-            boolean redCone;
-
-            {
-              Color.colorToHSV(camera.getARGB(x, scanlineY), hsv);
-              double hue = hsv[0];
-              redCone = !hueRange(hue, 220, 260);
-            }
+            boolean redCone = (positionAndHueRangeIndex.getSecond() == 1);// INFO: If the hue range is 1, then that indicates the first range, which is the hue range for RED.
+            nearestPointGuess = positionAndHueRangeIndex.getFirst();
+            depth = positionAndHueRangeIndex.getThird();
+            //DISABLED:
+            // - float depth = 100000000;
+            // - for (i = (int) (data.getWidth() * 0.2); i < data.getWidth() * 0.8; i++) {
+            // -   Color.colorToHSV(camera.getARGB(i, scanlineY), hsv);
+            // -   double hue = hsv[0];
+            // -   double sat = hsv[1];
+            // -   double val = hsv[2];
+            // -   if (camera.getDistance(i, scanlineY) != 0 && camera.getDistance(i, scanlineY) < depth) {
+            // -     if ((hueRange(hue, 220, 260) || hueRange(hue, 320, 10)) && sat > 0.3) {
+            // -       nearestPointGuess = i;
+            // -       depth = camera.getDistance(i, scanlineY);
+            // -     }
+            // -   }
+            // - }
 
             StringBuilder hueString = new StringBuilder();
             StringBuilder satString = new StringBuilder();
             StringBuilder valString = new StringBuilder();
 
 
-            for(i = x; i >= 0; i-=2){
+            for(i = nearestPointGuess; i >= 0; i-=2){
               Color.colorToHSV(camera.getARGB(i, scanlineY), hsv);
               double hue = hsv[0];
               double sat = hsv[1];
@@ -217,7 +224,7 @@ public class RooksLegacySmartMove extends LinearOpMode {
             notConeCountdown = notConeTolerance;
             if(leftSideCone == -1) continue;
 
-            for(i = x; i <= data.getWidth(); i+=2){
+            for(i = nearestPointGuess; i <= data.getWidth(); i+=2){
               Color.colorToHSV(camera.getARGB(i, scanlineY), hsv);
               double hue = hsv[0];
               double sat = hsv[1];
@@ -252,8 +259,11 @@ public class RooksLegacySmartMove extends LinearOpMode {
             int distanceFromMiddle = data.getWidth() / 2 - middleCone - 40;
             double degreesPerPixel = 90.0 / data.getWidth();
 
-            // Best fit equation
-            double pseudoDistance = 0.571 * Math.tan(Math.toRadians(-0.046 * (coneWidth - 53) + 90.0)) - 3.3;
+            // INFO: Best fit equation goes crazy after this point, plus we can't see a cone out that far.
+            if(coneWidth < 90) continue;
+
+            // INFO: Best fit equation
+            double pseudoDistance = 0.571 * Math.tan(Math.toRadians(-0.046 * (coneWidth - 53) + 90.0)) - 2.85; // Forward = less negative
 
             drive.updatePoseEstimate();
             drive.followTrajectory(drive.trajectoryBuilder(drive.getPoseEstimate()).forward(pseudoDistance).build());
@@ -265,9 +275,12 @@ public class RooksLegacySmartMove extends LinearOpMode {
             }
 
             camera.drawHorizontalLine(scanlineY);
-            camera.drawVerticalLine(middleCone);
+            //camera.drawVerticalLine(middleCone);
             camera.drawVerticalLine(leftSideCone);
             camera.drawVerticalLine(rightSideCone);
+            camera.drawVerticalLine(nearestPointGuess);
+            System.out.println("distance to detected cone point: "+depth);
+
             camera.transmitMonochromeImage();
 
             drive.turn(Math.toRadians(degreesPerPixel * distanceFromMiddle));
@@ -285,12 +298,19 @@ public class RooksLegacySmartMove extends LinearOpMode {
     } catch (NoFrameSetYetAcquiredException |
             UnsupportedStreamTypeException  |
             StreamTypeNotEnabledException   |
-            FrameQueueCloseException        |
             DisconnectedCameraException     |
             CameraStopException             |
             InterruptedException            |
             CameraStartException e) {
       e.printStackTrace();
+    } finally {
+      try {
+        camera.close();
+      } catch (FrameQueueCloseException e) {
+        e.printStackTrace();
+      } catch (CameraStopException e) {
+        e.printStackTrace();
+      }
     }
   }
 
@@ -314,5 +334,40 @@ public class RooksLegacySmartMove extends LinearOpMode {
   private boolean hueRange(double x, double from, double to){
     if(from < to) return range(x, from, to);
     return (range(x, from, 360) || range(x, 0, to));
+  }
+
+  private Triple<Integer, Integer, Float> getNearestObjectWithHueRange(int scanlineY, Pair<Float, Float>[] hueRanges) throws NoFrameSetYetAcquiredException, UnsupportedStreamTypeException, StreamTypeNotEnabledException {
+    int i;
+    FrameData data = camera.getImageFrame(StreamType.DEPTH);
+    int middleCone = -1;
+    float depth = 100000000;
+    int x = -1;
+
+    float[] hsv = new float[3];
+
+    int associatedColourRange = 0;
+
+    int avgCount=0;
+    for (i = (int) (data.getWidth() * 0.2); i < data.getWidth() * 0.8; i++) {
+      Color.colorToHSV(camera.getARGB(i, scanlineY), hsv);
+
+      double hue = hsv[0];
+      double sat = hsv[1];
+      double val = hsv[2];
+
+      float newDepth=camera.getDistance(i, scanlineY);
+      if (newDepth != 0 && newDepth <= depth) {
+        int m = 0;
+        for (Pair<Float, Float> hueRange : hueRanges) {
+          if (hueRange(hue, hueRange.fst, hueRange.snd) && sat > 0.3) {
+            x = i;
+            depth = newDepth;
+            associatedColourRange = m;
+          }
+          m++;
+        }
+      }
+    }
+    return new Triple<>(x, associatedColourRange, depth);
   }
 }
